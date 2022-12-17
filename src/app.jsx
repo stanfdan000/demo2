6493454ed61307afffd240565fdae32faed96c76
Daniel Stanford <97486451+stanfdan000@users.noreply.github.com>
2022-12-16 21:06:28 -0800

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,40 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import Layout from './components/Page/Layout.jsx';
+import Customers from './components/Sales/Customers.jsx';
+import Deposits from './components/Sales/Deposits.jsx';
+import Invoices from './components/Sales/Invoices.jsx';
+import Sales from './components/Sales/Sales.jsx';
+import Users from './components/Users/Users.jsx';
+import Auth from './components/Auth/Auth.jsx';
+import AuthForm from './components/Auth/AuthForm.jsx';
+  
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="users" element={<Users />} />
+          <Route path="sales" element={<Sales />}>
+            <Route index element={<Deposits />} />
+            <Route path="invoices" element={<Invoices />} />
+            <Route path="customers" element={<Customers />} />
+          </Route>
+        </Route>
+  
+        <Route path="auth" element={<Auth />}>
+          <Route index element={<AuthForm mode="signin" />} />
+          <Route path="signup" element={<AuthForm mode="signup" />} />
+        </Route>
+  
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
